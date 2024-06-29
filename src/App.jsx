e0740844c06555523316e5a68d01b2729c6c9c60
Tushar Sharma <tushar.sh08@gmail.com>
2024-06-29 11:21:25 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Background from "./components/Background"
 import Front from './components/Front'
+
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-800'>
